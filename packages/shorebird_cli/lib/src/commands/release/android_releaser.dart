d49d7ea6f8509fbcc2784a8105384a4341e7bcae
Erick Zanardo <erickzanardoo@gmail.com>
2024-06-21 15:41:33 -0300

    changing tracers

--- packages/shorebird_cli/lib/src/commands/release/android_releaser.dart
+++ packages/shorebird_cli/lib/src/commands/release/android_releaser.dart
@@ -113,8 +113,6 @@ Please comment and upvote ${link(uri: Uri.parse('https://github.com/shorebirdtec
     ShorebirdTracer.startTracing();
     await artifactBuilder.runPubget();
     ShorebirdTracer.endTracing('runPubget');
-
-    ShorebirdTracer.startTracing();
     try {
       aab = await artifactBuilder.buildAppBundle(
         flavor: flavor,
@@ -127,7 +125,6 @@ Please comment and upvote ${link(uri: Uri.parse('https://github.com/shorebirdtec
       buildAppBundleProgress.fail(e.message);
       throw ProcessExit(ExitCode.software.code);
     }
-    ShorebirdTracer.endTracing('buildAppBundle');
 
     buildAppBundleProgress.complete();
 
